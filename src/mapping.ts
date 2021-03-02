efa2a3b0e64cef27ccfbcb7b6fe695a69028db42
Simon Kruse <kruse.simon@gmx.net>
2021-03-02 15:23:51 +0100

    First commit

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,4 +1,4 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { BigInt, Address, store } from "@graphprotocol/graph-ts";
 import {
   PolkaPet,
   WhitelistAdminAdded,
@@ -9,67 +9,12 @@ import {
   URI,
   TransferSingle,
   TransferBatch,
-  ApprovalForAll
-} from "../generated/PolkaPet/PolkaPet"
-import { ExampleEntity } from "../generated/schema"
+  ApprovalForAll,
+} from "../generated/PolkaPet/PolkaPet";
+import { Pet, PetHolder } from "../generated/schema";
+import { BIGINT_ZERO, ZERO_ADDRESS } from "./constants";
 
-export function handleWhitelistAdminAdded(event: WhitelistAdminAdded): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
-
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (entity == null) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.account = event.params.account
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.tokenMaxSupply(...)
-  // - contract.balanceOf(...)
-  // - contract.supportsInterface(...)
-  // - contract.name(...)
-  // - contract.uri(...)
-  // - contract.tokenSupply(...)
-  // - contract.balanceOfBatch(...)
-  // - contract.maxSupply(...)
-  // - contract.owner(...)
-  // - contract.isOwner(...)
-  // - contract.symbol(...)
-  // - contract.isMinter(...)
-  // - contract.create(...)
-  // - contract.isWhitelistAdmin(...)
-  // - contract.totalSupply(...)
-  // - contract.creators(...)
-  // - contract.contractURI(...)
-  // - contract.isApprovedForAll(...)
-}
+export function handleWhitelistAdminAdded(event: WhitelistAdminAdded): void {}
 
 export function handleWhitelistAdminRemoved(
   event: WhitelistAdminRemoved
@@ -81,10 +26,86 @@ export function handleMinterRemoved(event: MinterRemoved): void {}
 
 export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
 
+/* export function handleURI(event: URI): void {} */
+
+export function handleTransferSingle(event: TransferSingle): void {
+  transferBase(
+    event.params._from,
+    event.params._to,
+    event.params._id,
+    event.params._amount,
+    event.block.timestamp
+  );
+}
+
+export function handleTransferBatch(event: TransferBatch): void {
+  if (event.params._ids.length != event.params._amounts.length) {
+    throw new Error("Inconsistent arrays length in TransferBatch");
+  }
+
+  for (let i = 0; i < event.params._ids.length; i++) {
+    let ids = event.params._ids;
+    let values = event.params._amounts;
+    transferBase(
+      event.params._from,
+      event.params._to,
+      ids[i],
+      values[i],
+      event.block.timestamp
+    );
+  }
+}
+
 export function handleURI(event: URI): void {}
 
-export function handleTransferSingle(event: TransferSingle): void {}
-
-export function handleTransferBatch(event: TransferBatch): void {}
-
 export function handleApprovalForAll(event: ApprovalForAll): void {}
+
+export function transferBase(
+  from: Address,
+  to: Address,
+  id: BigInt,
+  amount: BigInt,
+  timestamp: BigInt
+): void {
+  let petId = id.toString();
+  let pet = Pet.load(petId);
+  if (pet == null) {
+    pet = new Pet(petId);
+    pet.createdAt = timestamp;
+    pet.save();
+  }
+
+  if (from != ZERO_ADDRESS) {
+    updatePetHolder(petId, from, BIGINT_ZERO.minus(amount));
+  }
+  updatePetHolder(petId, to, amount);
+}
+
+export function updatePetHolder(
+  petId: string,
+  owner: Address,
+  deltaQuantity: BigInt
+): void {
+  let petHolderId = petId + "/" + owner.toHexString();
+  let petHolder = PetHolder.load(petHolderId);
+
+  if (petHolder == null) {
+    petHolder = new PetHolder(petHolderId);
+    petHolder.pet = petId;
+    petHolder.owner = owner;
+    petHolder.quantity = BIGINT_ZERO;
+  }
+
+  let newQuantity = petHolder.quantity.plus(deltaQuantity);
+
+  if (newQuantity.lt(BIGINT_ZERO)) {
+    throw new Error("Negative token quantity");
+  }
+
+  if (newQuantity.isZero()) {
+    store.remove("PetHolder", petHolderId);
+  } else {
+    petHolder.quantity = newQuantity;
+    petHolder.save();
+  }
+}
